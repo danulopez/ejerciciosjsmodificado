10aed56687d6a20ee71b44a9d6fe8f59f577766f
Daniela Lopez <daniela.lopez@bootcamp-upgrade.com>
2023-05-22 17:17:54 +0200

    jsmodific

--- S7/1.arrow-functions/ex1.js
+++ S7/1.arrow-functions/ex1.js
@@ -5,13 +5,10 @@
 //- Ejecuta esta función pasando un solo parametro
 //- Ejecuta esta función pasando dos parametros
 
-const num = (a = 10, b = 5) =>{
+const suma = (a = 10, b = 5) => {
     console.log(a + b);
 }
 
-//Crea una arrow function que tenga dos parametros a y b y que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre por consola la suma de los dos parametros. 
-
-//- Ejecuta esta función sin pasar ningún parametro
-//- Ejecuta esta función pasando un solo parametro
-//- Ejecuta esta función pasando dos parametros
-
+suma();
+suma(10);
+suma(10, 5);
